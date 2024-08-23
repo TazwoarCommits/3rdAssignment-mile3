dd1a82c2fdbe5b084064ebe11eba1d3087670943
TazwoarCommits <tazwoarlives@gmail.com>
2024-08-23 23:40:07 +0600

    Completed 80%

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    }
   }
+}
